81129c2f6ccbef7693e6d0d6184166186136eacf
Arcananix <43663274+Arcananix@users.noreply.github.com>
2019-02-05 01:08:52 -0600

    Create mod to remove daily ping

---
+++ RemoveDayPing/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Harmony;
+using System.Reflection;
+
+namespace RemoveDayPing
+{
+    public class Main
+    {
+        public static void Init(string directory, string settings)
+        {
+            HarmonyInstance.Create("Arcananix.RemoveDayPing").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ RemoveDayPing/SGTimePlayPause_SetDay_Patch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace RemoveDayPing
+{
+    [HarmonyPatch(typeof(SGTimePlayPause), "SetDay")]
+    public static class SGTimePlayPause_SetDay_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator iL)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            int startIndex = -1;
+            int endIndex = -1;
+            bool foundCheck = false;
+            bool foundPing = false;
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4 && (int)codes[i].operand == 235)
+                {
+                    foundCheck = true;
+                    startIndex = i;
+                }
+
+                if (foundCheck && codes[i].opcode == OpCodes.Ldc_I4_0)
+                {
+                    foundPing = true;
+                    endIndex = i;
+                }
+            }
+
+            if (foundPing)
+            {
+                codes.RemoveRange(startIndex, endIndex - startIndex);
+            }
+
+            return codes.AsEnumerable();
+        }
+    }
+
+    
+}
